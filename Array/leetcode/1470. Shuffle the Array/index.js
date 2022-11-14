06a0ae767720419304491a110a1ac7f78e66ba07
Ezaz Ahmed <ezaz.dev@gmail.com>
2022-11-14 09:41:27 +0600

    Array 1470 solved

---
+++ Array/leetcode/1470. Shuffle the Array/index.js
@@ -0,0 +1,14 @@
+const shuffle = function (nums, n) {
+	const result = [];
+
+	for (let i = 0; i < n; i++) {
+		result.push(nums[i]);
+		result.push(nums[n + i]);
+	}
+
+	return result;
+};
+
+console.log(shuffle([2, 5, 1, 3, 4, 7], 3)); // [2,3,5,4,1,7]
+console.log(shuffle([1, 2, 3, 4, 4, 3, 2, 1], 4)); //[1,4,2,3,3,2,4,1]
+console.log(shuffle([1, 1, 2, 2], 2)); //[1,2,1,2]
